5d6645a01456dce48b53f566c7c89448a76879c4
stefankrasnow <jocrednow@gmail.com>
2024-08-06 13:56:21 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Stepan Krasnov on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
